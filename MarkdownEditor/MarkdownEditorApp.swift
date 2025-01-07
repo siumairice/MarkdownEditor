9d1dd4670df7c8c6ad94dc75a9cff84c81400732
Nicole Go <nicoleam.go@gmail.com>
2025-01-07 17:38:04 -0500

    Initial Commit

---
+++ MarkdownEditor/MarkdownEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownEditorApp.swift
+//  MarkdownEditor
+//
+//  Created by Nicole Go on 2025-01-07.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarkdownEditorTests/MarkdownEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownEditorTests.swift
+//  MarkdownEditorTests
+//
+//  Created by Nicole Go on 2025-01-07.
+//
+
+import Testing
+@testable import MarkdownEditor
+
+struct MarkdownEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
